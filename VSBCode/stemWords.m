616a450769a83ea8cdaa0587c75f3d46dce81b7b
vidurbhatnagar <vidur.bhatnagar@gmail.com>
2015-11-25 16:55:43 -0500

    updated stemWords.m

--- VSBCode/stemWords.m
+++ VSBCode/stemWords.m
@@ -1,26 +1,27 @@
-function [mWordsCollapsed] = stemWords(cWordsActual, mWords)
+function [mFeaturesCollapsed] = stemWords(cWords, mFeatures)
 %% Stemming each word in cWordsActual
-mWordsStemmed = cell(size(cWordsActual));
-for iter = 1:length(cWordsActual)
-    mWordsStemmed{iter} = porterStemmer(cWordsActual{iter});
+cWordsStemmed = cell(size(cWords));
+for iter = 1:length(cWords)
+    cWordsStemmed{iter} = porterStemmer(cWords{iter});
 end
 
 %% Findind duplicate indices after stemming
-[~,uniqueIndices] = unique(mWordsStemmed);
-duplicateIndices = setdiff(1:length(mWordsStemmed), uniqueIndices)';
+[~,uniqueIndices] = unique(cWordsStemmed);
+duplicateIndices = setdiff(1:length(cWordsStemmed), uniqueIndices)';
 
 %% Collapsing Counts and Removing duplicates
-mWordsCollapsed = mWords;
+mFeaturesCollapsed = mFeatures;
 for iter= 1:length(duplicateIndices)
     % Find all common indices to this duplicate entry
-    commonIndices = find(strcmp(mWordsStemmed, mWordsStemmed{duplicateIndices(iter)}));
+    commonIndices = find(strcmp(cWordsStemmed, cWordsStemmed{duplicateIndices(iter)}));
     
     % Sum all entries in the first common index and set all other common
     % indices to 0 so that they don't sum up again
-    mWordsCollapsed(:,commonIndices(1)) = sum(mWordsCollapsed(:,commonIndices),2);
-    mWordsCollapsed(:,commonIndices(2:end)) = 0;
+    mFeaturesCollapsed(:,commonIndices(1)) = sum(mFeaturesCollapsed(:,commonIndices),2);
+    mFeaturesCollapsed(:,commonIndices(2:end)) = 0;
 end
 
 % Delete duplicate indices
-mWordsCollapsed(:,duplicateIndices) = [];
+mFeaturesCollapsed(:,duplicateIndices) = [];
 
+end
