7333b57b0369bf760df191f16750275e711761af
vidurbhatnagar <vidur.bhatnagar@gmail.com>
2015-12-01 22:25:45 -0500

    Updated Label Images

--- VSBCode/labelImages.m
+++ VSBCode/labelImages.m
@@ -3,15 +3,15 @@ close all;
 images = images_test;
 numImages = size(images,1);
 
-f = figure; 
-set(f,'KeyPressFcn',@KeyPressFcn) ;
-
-% labels = zeros(numImages,1);
-errorIndex = [3037,3040,3059,3159,3174,3204,3297];
-for iter = errorIndex
-    iter-2
-    labels(iter-2)
-    imshow(uint8(reshape(images(iter-2,:),[100 100 3])));
+labels = zeros(numImages,1);
+% errorIndex = [3037,3040,3059,3159,3174,3204,3297];
+for iter = 1:numImages
+    imshow(uint8(reshape(images(iter,:),[100 100 3])));
     [~,~,button] = ginput(1);
-    labels(iter-2) = button; 
+    
+    if button == 1
+        labels(iter) = 0;
+    else
+        labels(iter) = 1;
+    end
 end
