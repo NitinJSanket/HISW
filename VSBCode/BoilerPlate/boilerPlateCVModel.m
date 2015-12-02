72abe4c0a6c0343096421b432600d8e9d3c331a9
vidurbhatnagar <vidur.bhatnagar@gmail.com>
2015-12-02 07:03:21 -0500

    Boiler Plate Updated, can run 16 models at once now
    change numX value to run on different RDP...

--- VSBCode/BoilerPlate/boilerPlateCVModel.m
+++ VSBCode/BoilerPlate/boilerPlateCVModel.m
@@ -1,21 +1,47 @@
 %% Boiler Plate Code for Final Stretch
-function [bestModel, trainAcc, testAcc] = boilerPlateCVModel(trainX, trainY, rankedX, actualWords)
+trainX = mWordsTrain;
+imgFX = mImageFeaturesTrain;
+trainY = vGendersTrain;
+rankedX = mRankedFeatures;
+actualWords = mWordsActual;
     
-    %% Feature Selection/Reduction/Normalization/Standardization
+%% Feature Selection/Reduction/Normalization/Standardization
+
+% you can use following functions - 
+% standardizeFeatures,
+% freqRelFeatures, 
+% stemFeatures, 
+% stemFeaturesWithRanking
+% example: allTrainX{1} = stemWordsWithRanking(actualWords, trainX, rankedX, 3000);
+
+% Just change this numX from 5000 to 3000 to 1000
+% Life is Golden !!!
+numX = 1000;
+
+allTrainX = cell(16,1);
+allTrainX{1} = rankFeatures(trainX,rankedX,numX);
+allTrainX{2} = normFeatures(rankFeatures(trainX,rankedX,numX));
+allTrainX{3} = standardizeFeatures(rankFeatures(trainX,rankedX,numX));
+allTrainX{4} = standardizeFeatures(normFeatures(rankFeatures(trainX,rankedX,numX)));
+allTrainX{5} = stemFeaturesWithRanking(trainX,actualWords,rankedX,numX);
+allTrainX{6} = normFeatures(stemFeaturesWithRanking(trainX,actualWords,rankedX,numX));
+allTrainX{7} = standardizeFeatures(stemFeaturesWithRanking(trainX,actualWords,rankedX,numX));
+allTrainX{8} = standardizeFeatures(normFeatures(stemFeaturesWithRanking(trainX,actualWords,rankedX,numX)));
+allTrainX{9} = [rankFeatures(trainX,rankedX,numX),imgFX];
+allTrainX{10} = normFeatures([rankFeatures(trainX,rankedX,numX),imgFX]);
+allTrainX{11} = standardizeFeatures([rankFeatures(trainX,rankedX,numX),imgFX]);
+allTrainX{12} = standardizeFeatures(normFeatures([rankFeatures(trainX,rankedX,numX),imgFX]));
+allTrainX{13} = [stemFeaturesWithRanking(trainX,actualWords,rankedX,numX),imgFX];
+allTrainX{14} = normFeatures([stemFeaturesWithRanking(trainX,actualWords,rankedX,numX),imgFX]);
+allTrainX{15} = standardizeFeatures([stemFeaturesWithRanking(trainX,actualWords,rankedX,numX),imgFX]);
+allTrainX{16} = standardizeFeatures(normFeatures([stemFeaturesWithRanking(trainX,actualWords,rankedX,numX),imgFX]));
+
+selTrainY = trainY;
+trainAcc = zeros(size(allTrainX,1),1);
+testAcc = zeros(size(allTrainX,1),1);
+for iter = 1:size(allTrainX,1)
+    selTrainX = allTrainX{iter};
     
-    % you can use following functions - 
-    % standardizeFeatures,
-    % freqRelFeatures, 
-    % stemFeatures, 
-    % stemFeaturesWithRanking
-    % example: finalTrainX = stemWordsWithRanking(actualWords, trainX, rankedX, 3000);
-    selTrainX = SOME_OF_THE_ABOVE_FUNCTIONS(trainX);
-
-    %unless you need to change 0/1 labels to -1/1 or 1/2 or ...
-    selTrainY = trainY; 
-
-    %% Divide dataset into 2 parts - held-in for K-folds model, held-out for testing
-
     %Stratified CVPartition
     cvPartition = cvpartition(trainY,'Holdout',.20);
     heldInIndices = training(cvPartition,1);
@@ -27,41 +53,9 @@ function [bestModel, trainAcc, testAcc] = boilerPlateCVModel(trainX, trainY, ran
     heldOutX = selTrainX(heldOutIndices,:);
     heldOutY = selTrainY(heldOutIndices,:);
 
-    %% Build a 10-fold cross validated model on heldIn-indices and check error
-
-    %Stratified CVPartition
-    numFolds = 10;
-    cvPartition = cvpartition(heldInY,'KFold',numFolds);
-    allModels = cell(numFolds,1);
-    cvTestErrors = zeros(numFolds,1);
-
-    for iter = 1:numFolds
-        kfTrainIndices = training(cvPartition,iter);
-        kfTestIndices = test(cvPartition,iter);
-        kfTrainX = heldInX(kfTrainIndices,:);
-        kfTrainY = heldInY(kfTrainIndices);
-        kfTestX = heldInX(kfTestIndices,:);
-        kfTestY = heldInY(kfTestIndices);
-
-        %CHANGE THIS PART ACCORDING TO YOUR MODEL
-        allModels{iter} = mnrfit(kfTrainX,kfTrainY);
-        allModels{iter}(isnan(allModels{iter})) = 0;
-
-        % CHANGE THIS PART ACCORDING TO YOUR PREDICT FUNCTION
-        [~,predicted] = max(mnrval(allModels{iter},kfTestX),[],2);
-        cvTestErrors(iter) = mean(predicted~=kfTestY);
-    end
-
-    %% Select the Best Model
-    [~,modelIndex] = min(cvTestErrors);
-    bestModel = allModels{modelIndex};
-
-    %% Calculate Held-in Train Accuracy and Held-out Test Accuracy
-
-    % CHANGE THIS PART ACCORDING TO YOUR PREDICT FUNCTION
-    [~,predicted] = max(mnrval(bestModel,heldInX),[],2);
-    trainAcc = mean(predicted==heldInY);
-    [~,predicted] = max(mnrval(bestModel,heldOutX),[],2);
-    testAcc = mean(predicted==heldOutY);
-
+    model = fitensemble(heldInX,heldInY,...
+                 'RobustBoost',100,'Tree', 'nprint', 100);
+    
+    trainAcc(iter)  = mean(predict(model,heldInX)==heldInY);
+    testAcc(iter)  = mean(predict(model,heldOutX)==heldOutY);
 end
