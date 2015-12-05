6b87f56481d9d716a0de4e7f8cd8524298c55ca5
vidurbhatnagar <vidur.bhatnagar@gmail.com>
2015-12-05 18:34:09 -0500

    Code for submission - 4 models - proj_final

--- VSBCode/normalizeFeatures.m
+++
@@ -1,8 +0,0 @@
-function [mFeaturesNormalized] = normalizeFeatures(mFeatures)
-    iNumSamples = size(mFeatures,1);
-    mMeanFeatures = repmat(mean(mFeatures),[iNumSamples,1]);
-    mStdDevFeatures = repmat(std(mFeatures),[iNumSamples,1]);
-    
-    mFeaturesNormalized = (mFeatures-mMeanFeatures)./mStdDevFeatures;
-    mFeaturesNormalized(isnan(mFeaturesNormalized)) = 0;
-end
